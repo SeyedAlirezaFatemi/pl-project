20c63489fa609706cb1dae3230c319bcf05f0906
alireza <alirezafa@rocketmail.com>
2019-06-27 09:40:45 +0430

    :sparkles: AccountBlueprint real->integer.

--- Blueprints/AccountBlueprint.rkt
+++ Blueprints/AccountBlueprint.rkt
@@ -1,19 +1,19 @@
 (define-datatype Account Account?
   (an-account 
     (has-interest boolean?)
-    (fee real?)
-    (minimum-deposit real?) ; For opening new accounts. Checked after subtracting fee.
+    (fee integer?)
+    (minimum-deposit integer?) ; For opening new accounts. Checked after subtracting fee.
     (monthly boolean?) ; #t => monthly interest & #f => yearly interst
     ; Yearly interset is calculated based on the minimum amount of deposit in the year.
     (period integer?) ; After period months, there will be no more interest for this account.
     (renewable boolean?)
-    (interest-rate real?) ; Yearly
-    (credit real?)
+    (interest-rate integer?) ; Yearly
+    (credit integer?)
     (has-variable-interest boolean?)
     (span-for-increase integer?)
-    (increase-rate real?)
+    (increase-rate integer?)
     (has-cheque boolean?)
     (has-card boolean?)
-    (transfer-fee real?)     
+    (transfer-fee integer?)     
   )
 )
