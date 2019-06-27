0c1c132b95b8fa1b70821e7b4ab39e93b79e7534
alireza <alirezafa@rocketmail.com>
2019-06-27 09:41:43 +0430

    :pencil2: Fixed a typo.

--- Blueprints/AccountBlueprint.rkt
+++ Blueprints/AccountBlueprint.rkt
@@ -4,7 +4,7 @@
     (fee integer?)
     (minimum-deposit integer?) ; For opening new accounts. Checked after subtracting fee.
     (monthly boolean?) ; #t => monthly interest & #f => yearly interst
-    ; Yearly interset is calculated based on the minimum amount of deposit in the year.
+    ; Yearly interest is calculated based on the minimum amount of deposit in the year.
     (period integer?) ; After period months, there will be no more interest for this account.
     (renewable boolean?)
     (interest-rate integer?) ; Yearly
