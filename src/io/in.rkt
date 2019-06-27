55d3d39bb97461ea28a366f468250deef587c5a9
alireza <alirezafa@rocketmail.com>
2019-06-27 14:49:38 +0430

    :sparkles: Read File.

--- src/io/in.rkt
+++ src/io/in.rkt
@@ -1,10 +1,35 @@
 #lang racket
 
-
-(define (handle_input line)
-  (displayln line)
+(define read-file
+  (lambda (file-location)
+    (let ([lines '()])
+      (for ([line (file->lines file-location)])
+        (if (not (or (equal? line "\n") (equal? line "")))
+          (set! lines (append lines (list line)))
+          null
+        )
+      )
+      lines
+    )
+  )
 )
 
-(for ([line (file->lines "../samples/sample_input.txt")])
-  (handle_input line);(displayln line)
-)
+(write (read-file "../samples/sample_input.txt"))
+
+; (define handle-input
+;   (lambda ()
+;     (let* ([phase 0]
+;            [account-details '()]
+;            [loan-details '()]
+;            [commands '()])
+;       (for ([line (file->lines "../samples/sample_input.txt")])
+;         (cond )
+;         (if (eq? line "setup") 
+;           (set! phase 1)
+
+;         )
+
+;       )
+;     )
+;   )
+; )
