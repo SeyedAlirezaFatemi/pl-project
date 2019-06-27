e3b1cd7c6b8c9505f610344ac214067804810911
alireza <alirezafa@rocketmail.com>
2019-06-27 15:45:17 +0430

    :wrench: Trying to recognize commands.

--- src/io/in.rkt
+++ src/io/in.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "../commander/Command.rkt")
+
 (define read-file
   (lambda (file-location)
     (let ([lines '()])
@@ -16,6 +18,7 @@
 
 ; phase = 0 => setup
 ; phase = 1 => commands
+; Returns a list with two elements: (setup commands)
 (define categorize 
   (lambda (lines phase setup commands)
     (if (null? lines)
@@ -36,6 +39,25 @@
   )
 )
 
+(define prepare-commands
+  (lambda (raw-commands commands)
+    (if (null? raw-commands)
+      commands
+      (cond
+        [(regexp-match #rx"Time goes by" 
+          (car raw-commands))
+          (prepare-commands (cdr raw-commands) (append commands (list (time))))]
+
+        [else commands]
+      )
+    )
+  )
+)
+
 (let ([lines (read-file "../samples/sample_input.txt")])
-  (write (categorize lines 0 '() '()))
+  (define categorized (categorize lines 0 '() '()))
+    (let* ([setup (car categorized)]
+           [commands (cadr categorized)])
+      (prepare-commands commands '())      
+    )
 )
