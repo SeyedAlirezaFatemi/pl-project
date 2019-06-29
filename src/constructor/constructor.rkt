3d88d5a75c3cba77c26ddb3b810ada830b126c9d
alireza <alirezafa@rocketmail.com>
2019-06-29 14:20:20 +0430

    :bug: Fixed a bug in regexes.

--- src/constructor/constructor.rkt
+++ src/constructor/constructor.rkt
@@ -36,11 +36,11 @@
       (let* ([raw-command (car raw-commands)] [rest-raw-commands (cdr raw-commands)])
         (cond
           ; time-command
-          [(regexp-match #rx"Time goes by" raw-command)
+          [(regexp-match #rx"^Time goes by" raw-command)
             (analyse-commands rest-raw-commands (append analysed-commands (list (time-command))))
           ]
           ; new-account-command
-          [(regexp-match #px"Customer (\\d+) wants to create an account of type (\\d+). Customer [\\d+] wants to start with (\\d+) Tomans." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) wants to create an account of type (\\d+). Customer [\\d+] wants to start with (\\d+) Tomans." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands
                 (append analysed-commands (list (new-account-command (string->number (cadr match)) (string->number (caddr match)) (string->number (cadddr match)))))
@@ -48,61 +48,61 @@
             )
           ]
           ; deposit-command
-          [(regexp-match #px"Customer (\\d+) adds (\\d+) Tomans to his account." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) adds (\\d+) Tomans to his account." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (deposit-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; renewal-command
-          [(regexp-match #px"Customer (\\d+) requests renewal." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) requests renewal." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (renewal-command (string->number (cadr match))))))
             )
           ]
           ; cheque-command
-          [(regexp-match #px"Customer (\\d+) writes a cheque for (\\d+) Tomans." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) writes a cheque for (\\d+) Tomans." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (cheque-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; card-command
-          [(regexp-match #px"Customer (\\d+) spends (\\d+) Tomans via his card." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) spends (\\d+) Tomans via his card." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (card-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; transfer-command
-          [(regexp-match #px"Customer (\\d+) transfers (\\d+) Tomans." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) transfers (\\d+) Tomans." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (transfer-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; withdraw-command
-          [(regexp-match #px"Customer (\\d+) withdraws (\\d+) Tomans from his account." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) withdraws (\\d+) Tomans from his account." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (withdraw-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; close-command
-          [(regexp-match #px"Customer (\\d+) closes his account." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) closes his account." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (close-command (string->number (cadr match))))))
             )
           ]
           ; request-loan-command
-          [(regexp-match #px"Customer (\\d+) requests a loan of type (\\d+)." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) requests a loan of type (\\d+)." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (request-loan-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; pay-debt-command
-          [(regexp-match #px"Customer (\\d+) pays (\\d+) Tomans of his debt." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) pays (\\d+) Tomans of his debt." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (pay-debt-command (string->number (cadr match)) (string->number (caddr match))))))
             )
           ]
           ; withdraw-loan-command
-          [(regexp-match #px"Customer (\\d+) withdraws the loan." raw-command) =>
+          [(regexp-match #px"^Customer (\\d+) withdraws the loan." raw-command) =>
             (lambda (match)
               (analyse-commands rest-raw-commands (append analysed-commands (list (withdraw-loan-command (string->number (cadr match))))))
             )
@@ -125,13 +125,13 @@
       (let* ([setup (car setups)] [rest-setups (cdr setups)])
         (cond
           ; account-type-setup
-          [(regexp-match #px"Account type (\\d+)" setup) =>
+          [(regexp-match #px"^Account type (\\d+)" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-current-account-setup
-          [(regexp-match #px"current-account\\? (true|false)" setup) =>
+          [(regexp-match #px"^current-account\\? (true|false)" setup) =>
             (lambda (match)
               (if (equal? (cadr match) "true")
                 (analyse-setups rest-setups 0 loan-types account-types (append current (list #t)))
@@ -140,19 +140,19 @@
             )
           ]
           ; account-bank-fee-setup
-          [(regexp-match #px"bank-fee\\? (\\d+) Tomans" setup) =>
+          [(regexp-match #px"^bank-fee\\? (\\d+) Tomans" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-minimum-deposit-setup
-          [(regexp-match #px"minimum-deposit\\? (\\d+) Tomans" setup) =>
+          [(regexp-match #px"^minimum-deposit\\? (\\d+) Tomans" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-monthly-setup
-          [(regexp-match #px"monthly\\? (true|false)" setup) =>
+          [(regexp-match #px"^monthly\\? (true|false)" setup) =>
             (lambda (match)
               (if (equal? (cadr match) "true")
                 (analyse-setups rest-setups 0 loan-types account-types (append current (list #t)))
@@ -161,13 +161,13 @@
             )
           ]
           ; account-period-setup
-          [(regexp-match #px"period\\? (\\d+) months" setup) =>
+          [(regexp-match #px"^period\\? (\\d+) months" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-renewable-setup
-          [(regexp-match #px"renewable\\? (true|false)" setup) =>
+          [(regexp-match #px"^renewable\\? (true|false)" setup) =>
             (lambda (match)
               (if (equal? (cadr match) "true")
                 (analyse-setups rest-setups 0 loan-types account-types (append current (list #t)))
@@ -176,19 +176,19 @@
             )
           ]
           ; account-interest-rate-setup
-          [(regexp-match #px"interest-rate\\? (\\d+) percent" setup) =>
+          [(regexp-match #px"^interest-rate\\? (\\d+) percent" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-credit-setup
-          [(regexp-match #px"credit\\? (\\d+) units" setup) =>
+          [(regexp-match #px"^credit\\? (\\d+) units" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-variability-setup
-          [(regexp-match #px"variability\\? (true|false)" setup) =>
+          [(regexp-match #px"^variability\\? (true|false)" setup) =>
             (lambda (match)
               (if (equal? (cadr match) "true")
                 (analyse-setups rest-setups 0 loan-types account-types (append current (list #t)))
@@ -197,19 +197,19 @@
             )
           ]
           ; account-span-for-increase-setup
-          [(regexp-match #px"span-for-increase\\? (\\d+) months" setup) =>
+          [(regexp-match #px"^span-for-increase\\? (\\d+) months" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-increase-rate-setup
-          [(regexp-match #px"increase-rate\\? ([0-9]*\\.?[0-9]*) percent" setup) =>
+          [(regexp-match #px"^increase-rate\\? ([0-9]*\\.?[0-9]*) percent" setup) =>
             (lambda (match)
               (analyse-setups rest-setups 0 loan-types account-types (append current (list (string->number (cadr match)))))
             )
           ]
           ; account-has-cheque-setup
-          [(regexp-match #px"has-cheque\\? (true|false)" setup) =>
+          [(regexp-match #px"^has-cheque\\? (true|false)" setup) =>
             (lambda (match)
               (if (equal? (cadr match) "true")
                 (analyse-setups rest-setups 0 loan-types account-types (append current (list #t)))
@@ -218,7 +218,7 @@
             )
           ]
           ; account-has-card-setup
-          [(regexp-match #px"has-card\\? (true|false)" setup) =>
+          [(regexp-match #px"^has-card\\? (true|false)" setup) =>
             (lambda (match)
               (if (equal? (cadr match) "true")
                 (analyse-setups rest-setups 0 loan-types account-types (append current (list #t)))
@@ -227,7 +227,7 @@
             )
           ]
           ; account-transfer-fee-setup
-          [(regexp-match #px"transfer-fee\\? (\\d+) Tomans" setup) =>
+          [(regexp-match #px"^transfer-fee\\? (\\d+) Tomans" setup) =>
             (lambda (match)
               (let* ([id (car current)]
                      [has-interest (not (cadr current))]
