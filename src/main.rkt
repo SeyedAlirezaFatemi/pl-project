11d8b2293fd404b75e77ecd0de9e9f0f54ed7690
alireza <alirezafa@rocketmail.com>
2019-07-04 00:04:05 +0430

    read from input.

--- src/main.rkt
+++ src/main.rkt
@@ -6,7 +6,7 @@
 (require racket/pretty)
 
 (let ([file-path (read-line (current-input-port) 'any)])
-  (let ([input-data (analyse-input-file "samples/sample_input.txt")])
+  (let ([input-data (analyse-input-file file-path)])
     (pretty-display "File read successfully!")
     (let* ([loan-types (car input-data)]
            [account-types (cadr input-data)]
