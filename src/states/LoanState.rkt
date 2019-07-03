8cdc7f5326ba38b021485bc588f6c0a1a2e1d4f3
alireza <alirezafa@rocketmail.com>
2019-07-03 14:15:55 +0430

    LoanState Extractors.

--- src/states/LoanState.rkt
+++ src/states/LoanState.rkt
@@ -4,8 +4,38 @@
 
 (define-datatype LoanState LoanState?
   (a-loan-state
-    (time integer?)
+    (time integer?) ; Start
     (type integer?)
     (debt integer?)
+  )
+)
+
+(define loan-state->time
+  (lambda (loan-state)
+    (cases LoanState loan-state
+      (a-loan-state (time type debt)
+        time
+      )
+    )
+  )
+)
+
+(define loan-state->type
+  (lambda (loan-state)
+    (cases LoanState loan-state
+      (a-loan-state (time type debt)
+        type
+      )
+    )
+  )
+)
+
+(define loan-state->debt
+  (lambda (loan-state)
+    (cases LoanState loan-state
+      (a-loan-state (time type debt)
+        debt
+      )
+    )
   )
 )
